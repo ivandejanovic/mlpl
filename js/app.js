d197e0d3be629da30c0bf32e8f5fd9b3854d0770
ivan.abrash <ivan.abrash@gmail.com>
2016-05-17 17:09:33 +0200

    pocetni bootstrap dizajn

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 (function(){
-	
+	$(document).ready(function(){
+        var languages = [{locale:'sr', label:'srpski'},{locale:'en', label:'english'}];
+        var selectedLanguage = 0;
+
+        $('#selectedLang').html(languages[selectedLanguage].label);
+
+        $('#languagePicker > li > a').click(function(){
+                var targetLang = $(this).attr('data-lang'),
+                    targetDivID = '#content_' + targetLang,
+                    parentLi = $(this).parent('li');
+
+                if (!parentLi.hasClass('active')){
+                    selectedLanguage = languages.indexOf(targetDivID);
+                    parentLi.addClass('active').siblings().removeClass('active');
+                    $(targetDivID).toggleClass('in').siblings().removeClass('in');
+                }
+            }
+        );
+
+    });
 }())
