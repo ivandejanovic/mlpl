88cdb9a367a544131648f11c89c44d16086ad4a1
Ivan Dejanovic <idejanovic@gmail.com>
2016-06-01 20:42:23 +0200

    Refactored parser.

--- src/mlpl/parse/parse.go
+++ src/mlpl/parse/parse.go
@@ -63,6 +63,146 @@ const (
 	done
 )
 
+type parseBuffer struct {
+	lineno int
+	reader *bufio.Reader
+}
+
+func (buffer *parseBuffer) getToken(reserved []types.ReservedWord) types.Token {
+	var currentToken types.TokenType
+	var currentTokenString string
+	var currentTokenRunes []rune
+
+	for state := start; state != done; {
+		save := true
+		r, _, err := buffer.reader.ReadRune()
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+
+		if r == newLine {
+			buffer.lineno++
+		}
+		switch state {
+		case start:
+			if unicode.IsDigit(r) {
+				state = inNum
+			} else if unicode.IsLetter(r) {
+				state = inId
+			} else if r == colon {
+				state = inAssign
+			} else if r == space || r == tab || r == newLine {
+				save = false
+			} else if r == numberSign {
+				save = false
+				state = inComment
+			} else if r == quotation {
+				save = false
+				state = inString
+			} else {
+				state = done
+				if err == io.EOF {
+					save = false
+					currentToken = types.ENDFILE
+				} else {
+					switch r {
+					case equal:
+						currentToken = types.EQ
+					case lt:
+						currentToken = types.LT
+					case plus:
+						currentToken = types.PLUS
+					case minus:
+						currentToken = types.MINUS
+					case times:
+						currentToken = types.TIMES
+					case over:
+						currentToken = types.OVER
+					case lParen:
+						currentToken = types.LPAREN
+					case rParen:
+						currentToken = types.RPAREN
+					case semi:
+						currentToken = types.SEMI
+					default:
+						currentToken = types.ERROR
+					}
+				}
+			}
+		case inComment:
+			save = false
+			if err == io.EOF {
+				state = done
+				currentToken = types.ENDFILE
+			} else if r == numberSign {
+				state = start
+			}
+		case inString:
+			if r == quotation {
+				save = false
+				state = done
+				currentToken = types.STRING
+			} else {
+				save = true
+			}
+		case inAssign:
+			state = done
+			if r == equal {
+				currentToken = types.ASSIGN
+			} else {
+				err = buffer.reader.UnreadRune()
+				if err != nil {
+					panic(err)
+				}
+				save = false
+				currentToken = types.ERROR
+			}
+		case inNum:
+			if !unicode.IsDigit(r) {
+				err = buffer.reader.UnreadRune()
+				if err != nil {
+					panic(err)
+				}
+				save = false
+				state = done
+				currentToken = types.NUM
+			}
+		case inId:
+			if !unicode.IsLetter(r) {
+				err = buffer.reader.UnreadRune()
+				if err != nil {
+					panic(err)
+				}
+				save = false
+				state = done
+				currentToken = types.ID
+			}
+		case done:
+			//Should never happen
+			fmt.Printf("Scanner bug: state= %d\n", state)
+			state = done
+			currentToken = types.ERROR
+		default:
+			//Should never happen
+			fmt.Printf("Scanner bug: state= %d\n", state)
+			state = done
+			currentToken = types.ERROR
+		}
+
+		if save {
+			currentTokenRunes = append(currentTokenRunes, r)
+		}
+		if state == done {
+			currentTokenString = string(currentTokenRunes)
+			if currentToken == types.ID {
+				currentToken = reservedLookup(currentTokenString, reserved)
+			}
+		}
+	}
+
+	return types.Token{currentToken, currentTokenString, buffer.lineno}
+}
+
 func reservedLookup(s string, reserved []types.ReservedWord) types.TokenType {
 	for index := 0; index < len(reserved); index++ {
 		if s == reserved[index].Str {
@@ -75,7 +215,6 @@ func reservedLookup(s string, reserved []types.ReservedWord) types.TokenType {
 
 func Parse(sourceFile string, reserved []types.ReservedWord) []types.Token {
 	var tokens []types.Token
-	lineno := 0
 
 	source, err := os.Open(sourceFile)
 	if err != nil {
@@ -83,144 +222,10 @@ func Parse(sourceFile string, reserved []types.ReservedWord) []types.Token {
 	}
 
 	reader := bufio.NewReader(source)
-
-	getToken := func(reserved []types.ReservedWord) types.Token {
-		var currentToken types.TokenType
-		var currentTokenString string
-		var currentTokenRunes []rune
-
-		for state := start; state != done; {
-			save := true
-			r, _, err := reader.ReadRune()
-			if err != nil && err != io.EOF {
-				panic(err)
-			}
-
-			if r == newLine {
-				lineno++
-			}
-			switch state {
-			case start:
-				if unicode.IsDigit(r) {
-					state = inNum
-				} else if unicode.IsLetter(r) {
-					state = inId
-				} else if r == colon {
-					state = inAssign
-				} else if r == space || r == tab || r == newLine {
-					save = false
-				} else if r == numberSign {
-					save = false
-					state = inComment
-				} else if r == quotation {
-					save = false
-					state = inString
-				} else {
-					state = done
-					if err == io.EOF {
-						save = false
-						currentToken = types.ENDFILE
-					} else {
-						switch r {
-						case equal:
-							currentToken = types.EQ
-						case lt:
-							currentToken = types.LT
-						case plus:
-							currentToken = types.PLUS
-						case minus:
-							currentToken = types.MINUS
-						case times:
-							currentToken = types.TIMES
-						case over:
-							currentToken = types.OVER
-						case lParen:
-							currentToken = types.LPAREN
-						case rParen:
-							currentToken = types.RPAREN
-						case semi:
-							currentToken = types.SEMI
-						default:
-							currentToken = types.ERROR
-						}
-					}
-				}
-			case inComment:
-				save = false
-				if err == io.EOF {
-					state = done
-					currentToken = types.ENDFILE
-				} else if r == numberSign {
-					state = start
-				}
-			case inString:
-				if r == quotation {
-					save = false
-					state = done
-					currentToken = types.STRING
-				} else {
-					save = true
-				}
-			case inAssign:
-				state = done
-				if r == equal {
-					currentToken = types.ASSIGN
-				} else {
-					err = reader.UnreadRune()
-					if err != nil {
-						panic(err)
-					}
-					save = false
-					currentToken = types.ERROR
-				}
-			case inNum:
-				if !unicode.IsDigit(r) {
-					err = reader.UnreadRune()
-					if err != nil {
-						panic(err)
-					}
-					save = false
-					state = done
-					currentToken = types.NUM
-				}
-			case inId:
-				if !unicode.IsLetter(r) {
-					err = reader.UnreadRune()
-					if err != nil {
-						panic(err)
-					}
-					save = false
-					state = done
-					currentToken = types.ID
-				}
-			case done:
-				//Should never happen
-				fmt.Printf("Scanner bug: state= %d\n", state)
-				state = done
-				currentToken = types.ERROR
-			default:
-				//Should never happen
-				fmt.Printf("Scanner bug: state= %d\n", state)
-				state = done
-				currentToken = types.ERROR
-			}
-
-			if save {
-				currentTokenRunes = append(currentTokenRunes, r)
-			}
-			if state == done {
-				currentTokenString = string(currentTokenRunes)
-				if currentToken == types.ID {
-					currentToken = reservedLookup(currentTokenString, reserved)
-				}
-			}
-		}
-
-		return types.Token{currentToken, currentTokenString, lineno}
-	}
+	buffer := &parseBuffer{0, reader}
 
 	for moreTokens := true; moreTokens; {
-		token := getToken(reserved)
+		token := buffer.getToken(reserved)
 		tokens = append(tokens, token)
 
 		if token.TokenType == types.ENDFILE {
